f069b49ac64e64f2da3cece3673ad8184f51b0a8
JETAN-Coder <jetanreddy@gmail.com>
2024-09-11 10:21:26 -0400

    interactjs

---
+++ index.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/rFyggwozwKESg4ty/scene.splinecode" />
+    </main>
+  );
+}
