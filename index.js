a87cc53dbac0fb31189a737b75668852e3efb16a
JETAN-Coder <jetanreddy@gmail.com>
2024-09-19 17:19:59 -0400

    life

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import Spline from '@splinetool/react-spline';
+import React, { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const cursor = document.querySelector('.cursor');
+
+    const moveCursor = (e) => {
+      cursor.style.transform = `translate(${e.pageX - 15}px, ${e.pageY - 15}px)`;
+    };
+
+    document.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      document.removeEventListener('mousemove', moveCursor);
+    };
+  }, []);
+
   return (
     <main>
       <Spline scene="https://prod.spline.design/rFyggwozwKESg4ty/scene.splinecode" />
     </main>
   );
 } 
+
