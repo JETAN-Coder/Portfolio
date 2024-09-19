f8b090c469d8e90659a5956ab5af9315e1014e07
JETAN-Coder <jetanreddy@gmail.com>
2024-09-19 17:45:05 -0400

    l

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 import Spline from '@splinetool/react-spline';
-import React, { useEffect } from 'react';
-import './styles.css'; // Make sure to import the CSS
+
+(function() {
+  console.clear();
+  
+  const follower = document.querySelector('#cursor');
+  
+  let posX = 0;
+  let posY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
+  
+  const ease = 0.1;
+  
+  function easeTo() {    
+    const followerBounds = follower.getBoundingClientRect();
+    
+    const dX = mouseX - (followerBounds.left + 16);
+    const dY = mouseY - (followerBounds.top + 16);
+    
+    posX += dX * ease;
+    posY += dY * ease;
+  }
+  
+  function update() {
+    easeTo();
+    follower.style.transform = `translate3d(${posX}px, ${posY}px, 0)`;  
+    requestAnimationFrame(update);
+  }
+  
+  function setCoords(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  }
+  
+	document.onmousemove = setCoords;
+  update();
+})();
 
 export default function Home() {
-  useEffect(() => {
-    // Target the cursor div
-    const cursor = document.querySelector('.cursor');
-
-    const moveCursor = (e) => {
-      // Check if the cursor div exists
-      if (cursor) {
-        cursor.style.transform = `translate(${e.pageX - 15}px, ${e.pageY - 15}px)`;
-      }
-    };
-
-    // Add the event listener to track mouse movement
-    document.addEventListener('mousemove', moveCursor);
-
-    // Remove the event listener when the component is unmounted
-    return () => {
-      document.removeEventListener('mousemove', moveCursor);
-    };
-  }, []);
-
   return (
     <main>
-      {/* The circle cursor div */}
-      <div className="cursor"></div>
-      
-      {/* The Spline 3D scene */}
       <Spline scene="https://prod.spline.design/rFyggwozwKESg4ty/scene.splinecode" />
     </main>
   );
-}
-
+} 
