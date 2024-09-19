b45ff15b5516002e9315d38c55821a7f12f5af9d
JETAN-Coder <jetanreddy@gmail.com>
2024-09-19 17:31:04 -0400

    hope

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import Spline from '@splinetool/react-spline';
 import React, { useEffect } from 'react';
-import './styles.css';
+import './styles.css'; // Make sure to import the CSS file
 
 export default function Home() {
   useEffect(() => {
+    // Adding the mousemove event listener
     const cursor = document.querySelector('.cursor');
-
+    
     const moveCursor = (e) => {
-      cursor.style.transform = `translate(${e.pageX - 15}px, ${e.pageY - 15}px)`;
+      if (cursor) {
+        cursor.style.transform = `translate(${e.pageX - 15}px, ${e.pageY - 15}px)`;
+      }
     };
 
+    // Add event listener when the component mounts
     document.addEventListener('mousemove', moveCursor);
 
+    // Clean up event listener when the component unmounts
     return () => {
       document.removeEventListener('mousemove', moveCursor);
     };
@@ -20,8 +25,9 @@ export default function Home() {
   return (
     <main>
       <div className="cursor"></div>
+      
+      {/* Spline scene */}
       <Spline scene="https://prod.spline.design/rFyggwozwKESg4ty/scene.splinecode" />
     </main>
   );
-} 
-
+}
