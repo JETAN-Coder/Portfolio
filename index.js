a4594c9ceb40a3182633bc6c40f30c809d1ba4ce
JETAN-Coder <jetanreddy@gmail.com>
2024-09-19 18:00:36 -0400

    jet

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 import Spline from '@splinetool/react-spline';
 
+(function() {
+  console.clear();
+  
+  const follower = document.querySelector('#follower');
+  
+  let posX = 0;
+  let posY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
+  
+  const ease = 0.1;
+  
+  function easeTo() {    
+    const followerBounds = follower.getBoundingClientRect();
+    
+    const dX = mouseX - (followerBounds.left + 16);
+    const dY = mouseY - (followerBounds.top + 16);
+    
+    posX += dX * ease;
+    posY += dY * ease;
+  }
+  
+  function update() {
+    easeTo();
+    follower.style.transform = `translate3d(${posX}px, ${posY}px, 0)`;  
+    requestAnimationFrame(update);
+  }
+  
+  function setCoords(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  }
+  
+	document.onmousemove = setCoords;
+  update();
+})();
+
 export default function Home() {
   return (
     <main>
